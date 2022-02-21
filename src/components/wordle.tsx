6b137eb1ae4b50548ab19c6c574f22785a6939be
Alfalmi <pinooxd@gmail.com>
2022-02-20 20:09:16 -0700

    commit

---
+++ src/components/wordle.tsx
@@ -0,0 +1,10 @@
+import Box from "./box";
+import RowCompleted from "./rowCompleted";
+
+export default function Wordle() {
+  return (
+    <div>
+      <RowCompleted word="sabia" solution="sabio" />
+    </div>
+  );
+}
